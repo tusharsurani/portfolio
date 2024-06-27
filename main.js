7259b2c6ef5df60f573d8c95b85b223061131449
tusharsurani <137286868+tusharsurani@users.noreply.github.com>
2024-06-27 21:05:55 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text",{
+    strings:["Frontend Developer", "Java Developer", "python developer"],
+    typeSpeed : 10,
+    backSpeed : 100,
+    backDelay : 1000,
+    loop : true
+});
